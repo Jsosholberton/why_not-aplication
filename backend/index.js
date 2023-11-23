431aca3bb4d626c234f6765b57996788db702db8
Johnatan Ortiz <6403@holbertonstudents.com>
2023-11-22 22:41:36 -0500

    backend SSL

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,8 @@ import express from "express";
 import dotenv from "dotenv";
 import conectDB from "./config/db.js";
 import cors from "cors";
+import https from "https";
+import fs from "fs";
 
 import userRoutes from "./routes/userRoutes.js";
 import tournamentRoutes from "./routes/tournamentRoutes.js";
@@ -11,7 +13,6 @@ dotenv.config();
 const app = express();
 
 app.use(cors());
-
 app.use(express.json());
 
 conectDB();
@@ -19,6 +20,15 @@ conectDB();
 app.use("/api/users", userRoutes);
 app.use("/api/tournaments", tournamentRoutes);
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server running on: ${process.env.PORT}`);
+const privateKeyPath = "/etc/letsencrypt/live/johnatanortiz.tech/privkey.pem";
+const certificatePath = "/etc/letsencrypt/live/johnatanortiz.tech/fullchain.pem";
+
+const privateKey = fs.readFileSync(privateKeyPath, 'utf8');
+const certificate = fs.readFileSync(certificatePath, 'utf8');
+const credentials = { key: privateKey, cert: certificate };
+
+const httpsServer = https.createServer(credentials, app);
+
+httpsServer.listen(process.env.PORT, () => {
+  console.log(`Server running on https://localhost:${process.env.PORT}`);
 });
